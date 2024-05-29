1422548adb30abd21de159f9675eb938b7326aab
thiagofmp <160027666+thiagofmp@users.noreply.github.com>
2024-05-29 10:23:30 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('messageButton').addEventListener('click', function() {
-    var messageParagraph = document.getElementById('message');
-    if (messageParagraph.classList.contains('hidden')) {
-    messageParagraph.classList.remove('hidden');
-    } else {
-    messageParagraph.classList.add('hidden');
-    }
-    });
